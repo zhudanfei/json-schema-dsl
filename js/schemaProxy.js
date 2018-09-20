a2282f9e17bbe0a4e9472abff1e38a852b8701ed
Fred Zhu <fred.zhu@openedgepay.com>
2018-09-20 14:52:20 -0700

    Works now.

--- js/schemaProxy.js
+++ js/schemaProxy.js
@@ -1,11 +1,9 @@
-function getter(obj, path){
-    for (const name of path){
-        if (obj[name] === undefined || obj[name] === null){
-            return null;
-        }
-        obj = obj[name];
+function convertToInteger(s){
+    const result = Number(s);
+    if (Number.isNaN(result)){
+        return null;
     }
-    return obj;
+    return result;
 }
 
 function findField(schemaObject, fieldName){
@@ -17,66 +15,56 @@ function findField(schemaObject, fieldName){
     return null;
 }
 
-function createProxy(schemaObject, path = [], hasUndefined = false){
-    return new Proxy(function(obj){}, new SchemaProxy(schemaObject, path, hasUndefined));
+function getter(schemaObj, path, obj){
+    for (const name of path){
+        if (schemaObj.type === 'Object'){
+            const field = findField(schemaObj, name);
+            if (field === null){
+                return undefined;
+            }
+            if (obj[name] === undefined || obj[name] === null){
+                return null;
+            }
+            schemaObj = field.fieldType;
+            obj = obj[name];
+        } else if (schemaObj.type === 'Array'){
+            const index = convertToInteger(name);
+            if (index === null){
+                return undefined;
+            }
+            schemaObj = schemaObj.elementType;
+            obj = obj[index];
+        } else if (schemaObj.type === 'StringMap') {
+            schemaObj = {};
+            obj = obj[name];
+        } else {
+            throw new Error('Path is too long')
+        }
+    }
+    return obj;
+}
+
+function createProxy(schemaObject, path = []){
+    return new Proxy(function(obj){}, new SchemaProxy(schemaObject, path));
 }
 
 class SchemaProxy {
-    constructor(schemaObject, path, hasUndefined){
+    constructor(schemaObject, path){
         this.$schemaObject = schemaObject;
         this.$path = path;
-        this.$hasUndefined = hasUndefined;
-    }
-
-    getObjectFieldValue(target, field){
-        const newPath = this.$path.concat([field.name]);
-        this[field.name] = createProxy(field.fieldType, newPath);
-        return this[field.name];
-    }
-
-    getArrayFieldValue(target, field){
-        return null;
-    }
-
-    getStringMapFieldValue(target, field){
-        return null;
-    }
-
-    getBasicFieldValue(target, field){
-        const newPath = this.$path.concat([field.name]);
-        this[field.name] = createProxy(field.fieldType, newPath);
-        return this[field.name];
-    }
-
-    getFieldValue(target, field){
-        if (field.fieldType.type === 'Object'){
-            return this.getObjectFieldValue(target, field);
-        } else if (field.fieldType.type === 'Array'){
-            return this.getArrayFieldValue(target, field);
-        } else if (field.fieldType.type === 'StringMap'){
-            return this.getStringMapFieldValue(target, field);
-        } else {
-            return this.getBasicFieldValue(target, field);
-        }
     }
 
     get(target, fieldName, receiver){
         if (this[fieldName] !== undefined){
             return this[fieldName];
         }
-        const field = findField(this.$schemaObject, fieldName);
-        if (field === null){
-            const newPath = this.$path.concat([fieldName]);
-            return createProxy({fields:[]}, newPath, true);
-        }
-        return this.getFieldValue(target, field);
+        const newPath = this.$path.concat([fieldName]);
+        this[fieldName] = createProxy(this.$schemaObject, newPath);
+        return this[fieldName];
     }
 
     apply(target, thisArg, argumentsList){
-        if (this.$hasUndefined){
-            return undefined;
-        }
-        return getter(argumentsList[0], this.$path);
+        return getter(this.$schemaObject, this.$path, argumentsList[0]);
     }
 }
 
@@ -84,27 +72,3 @@ module.exports = {
     createProxy: createProxy
 };
 
-/*
-var obj = {node: 'abc', user: ['def', null, 'f'], tag: {name: null, level: 2}};
-console.log(getter(obj, ['use', '2']));
-
-function processField(field, path) {
-    const newPath = path.concat([field.name]);
-    let flattenedFields = [CreateFieldContext(newPath)];
-    const classType = field.fieldType;
-    flattenedFields += flattenFields(field.GetClass(), newPath);
-    return flattenedFields
-}
-
-function flattenFields(jsonObject, path) {
-    let flattenedFields = [];
-    for (field of jsonObject.fields) {
-        flattenedFields = flattenedFields.concat(processField(field, path));
-    }
-    return flattenedFields;
-}
-
-function schemaProxy(jsonObject){
-
-}
-*/
--- js/test/schemaProxyTest.js
+++ js/test/schemaProxyTest.js
@@ -15,18 +15,30 @@ const schema1 = JsonObject(
             )
         )
     )),
-    JsonField('event', JsonArray(JsonObject(JsonField('name', JsonString),
-        JsonField('alarm', JsonBoolean)
-    )))
+    JsonField('event', JsonArray(
+        JsonObject(
+            JsonField('name', JsonString),
+            JsonField('alarm', JsonBoolean)
+        )
+        )
+    ),
+    JsonField('spec', JsonStringMap),
 );
 
-describe('Schema 1 proxy', function() {
+describe('Schema 1 proxy', function () {
     const proxy = schemaProxy.createProxy(schema1);
     it('Should return value in the first level', function () {
         const data = {node: '5'};
         const expected = '5';
         const actual = proxy.node(data);
         assert.equal(actual, expected);
+    });
+
+    it('Should return whole object in the first level', function () {
+        const data = {node: '5', tag: {name: 'abc', level: 10}};
+        const expected = {name: 'abc', level: 10};
+        const actual = proxy.tag(data);
+        assert.deepEqual(actual, expected);
     });
 
     it('Should return undefined if the name is not in the schema first level', function () {
@@ -52,6 +64,11 @@ describe('Schema 1 proxy', function() {
         const expected = 10;
         const actual = proxy.tag.level(data);
         assert.equal(actual, expected);
+    });
+
+    it('Should throw error if there is no second level', function () {
+        const data = {node: '5', tag: {name: 'abc', level: 10}};
+        assert.throws(() =>  proxy.tag.level.x(data), Error, "Path is too long");
     });
 
     it('Should return undefined if the name is not in the schema second level', function () {
@@ -84,4 +101,49 @@ describe('Schema 1 proxy', function() {
         assert.isNull(actual);
     });
 
+    it('Should return value in the array', function () {
+        const data = {node: '5', user: ['abc', 'xyz']};
+        const expected = 'xyz';
+        const actual = proxy.user[1](data);
+        assert.equal(actual, expected);
+    });
+
+    it('Should return undefined if name is string in the array', function () {
+        const data = {node: '5', user: ['abc', 'xyz']};
+        const actual = proxy.user.a(data);
+        assert.isUndefined(actual);
+    });
+
+    it('Should return value in the array of object', function () {
+        const data = {node: '5', event: [{name: 'abc'}, {name: 'xyz'}]};
+        const expected = 'xyz';
+        const actual = proxy.event[1].name(data);
+        assert.equal(actual, expected);
+    });
+
+    it('Should return undefined if name is string in the array', function () {
+        const data = {node: '5', event: [{name: 'abc'}, {name: 'xyz'}]};
+        const actual = proxy.event.x.name(data);
+        assert.isUndefined(actual);
+    });
+
+    it('Should return undefined if path is longer', function () {
+        const data = {node: '5', event: [{name: 'abc'}, {name: 'xyz'}]};
+        assert.throws(() =>  proxy.event[0].name.abc(data), Error, "Path is too long");
+    });
+
+    it('Should return undefined if value is missing in the array of object', function () {
+        const data = {node: '5', event: [{name: 'abc'}, {name: 'xyz'}]};
+        const actual = proxy.event[0].nam(data);
+        assert.isUndefined(actual);
+    });
+
+    it('Should return value in a string map', function () {
+        const data = {node: '5', spec: {def:'1', size:'xyz'}};
+        const expected = 'xyz';
+        const actual = proxy.spec.size(data);
+        assert.equal(actual, expected);
+    });
+
 });
+
